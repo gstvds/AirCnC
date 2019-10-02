8d9f89502242a66261ee9950d65d649680fa992b
Gustavo da Silva <gstvds@icloud.com>
2019-10-02 12:18:51 -0300

    Created frontend

---
+++ frontend/src/components/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+
+import logo from "../assets/logo.svg";
+
+function App() {
+  return (
+    <div className="container">
+      <img src={logo} alt="Logo" />
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre{" "}
+          <strong>talentos</strong> para sua empresa
+        </p>
+
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input type="email" id="email" placeholder="Seu melhor e-mail" />
+
+          <button type="submit">ENVIAR</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
