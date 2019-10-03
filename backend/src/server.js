033aa6b66278ea80f703413dd487e0d45e7218fe
Gustavo da Silva <gstvds@icloud.com>
2019-10-02 21:54:27 -0300

    Changed routes to routes.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const routes = require("./routes.js");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
-});
+// req.query = acessar os query params (para filtros)
+// req.params = acessar route params (para edição, delete)
+// req.body = acessar corpo da requisição (para criação, edição)
+
+app.use(express.json()); // Indica para o express utilizar JSON
+app.use(routes); // Necessita vir depois do express.json, pois a requisição do json precisa ser feita antes
 
 app.listen(3333);
