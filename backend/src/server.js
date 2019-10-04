74711cc0d97fb558ede8eb303d5ed3371642db43
Gustavo da Silva <gstvds@icloud.com>
2019-10-04 14:22:48 -0300

    Added image preview feature

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes.js");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -16,7 +18,9 @@ mongoose.connect(
 // req.params = acessar route params (para edição, delete)
 // req.body = acessar corpo da requisição (para criação, edição)
 
+app.use(cors());
 app.use(express.json()); // Indica para o express utilizar JSON
+app.use("/files", express.static(path.resolve(__dirname, "..", "/uploads")));
 app.use(routes); // Necessita vir depois do express.json, pois a requisição do json precisa ser feita antes
 
 app.listen(3333);
