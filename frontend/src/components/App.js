1a3075402d4822882f8fd056b7aab6aa4d0b8a37
Gustavo da Silva <gstvds@icloud.com>
2019-10-04 14:22:10 -0300

    Web app finished

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,33 +1,17 @@
 import React from "react";
-import api from "../services/api.js";
 import "./App.css";
 
 import logo from "../assets/logo.svg";
 
-function App() {
-  function handleSubmit(event) {
-    event.preventDefault();
+import Routes from "../routes";
 
-    console.log("Hello World");
-  }
+function App() {
   return (
     <div className="container">
       <img src={logo} alt="Logo" />
 
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre{" "}
-          <strong>talentos</strong> para sua empresa
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input type="email" id="email" placeholder="Seu melhor e-mail" />
-
-          <button className="btn" type="submit">
-            ENVIAR
-          </button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
